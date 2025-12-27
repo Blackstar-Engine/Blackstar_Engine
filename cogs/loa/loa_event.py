efcd670403bd24e2694b4e791cf3fb812818a130
JustPhantom50 <tjherm4441@gmail.com>
2025-12-27 04:09:50 +0000

    fixed loa event loop

--- cogs/loa/loa_event.py
+++ cogs/loa/loa_event.py
@@ -1,33 +1,98 @@
 import discord
 from discord.ext import commands, tasks
-from utils.constants import loa, loa_channel
-from datetime import datetime
+from datetime import datetime, timezone
+
+from utils.constants import (
+    loa,
+    stored_loa,
+    loa_channel,
+    loa_role,
+)
+
 
 class LOAEvent(commands.Cog):
     def __init__(self, bot: commands.Bot):
         self.bot = bot
-    
+        self.check_loa_end_date.start()
+
     @tasks.loop(minutes=1)
     async def check_loa_end_date(self):
-        all_active_loas = await loa.find().to_list(length=None)
+        now = datetime.now(timezone.utc)
 
-        for loa_record in all_active_loas:
-            current_datetime = datetime.now()
+        # Only fetch expired LOAs
+        expired_loas = await loa.find(
+            {"end_date": {"$lte": now}}
+        ).to_list(length=None)
 
-            guild: discord.Guild = await self.bot.get_guild(loa_record.get('guild_id'))
-            channel: discord.TextChannel = await guild.fetch_channel(loa_channel)
-            user: discord.User = await self.bot.fetch_user(loa_record.get('user_id'))
+        for record in expired_loas:
+            guild = self.bot.get_guild(record.get("guild_id"))
+            if not guild:
+                await self._cleanup_record(record)
+                continue
 
-            if current_datetime > loa_record.get('end_date'):
-                log_embed = discord.Embed(
+            # Channel (cached first)
+            channel = guild.get_channel(loa_channel)
+            if not channel:
+                try:
+                    channel = await guild.fetch_channel(loa_channel)
+                except discord.NotFound:
+                    await self._cleanup_record(record)
+                    continue
+
+            member = guild.get_member(record["user_id"])
+            user = member or self.bot.get_user(record["user_id"])
+
+            if not user:
+                try:
+                    user = await self.bot.fetch_user(record["user_id"])
+                except discord.NotFound:
+                    await self._cleanup_record(record)
+                    continue
+
+            # Remove LOA role
+            role = guild.get_role(loa_role)
+            if role and member:
+                try:
+                    await member.remove_roles(
+                        role,
+                        reason="LOA expired"
+                    )
+                except discord.Forbidden:
+                    pass
+
+            # Log embed
+            embed = discord.Embed(
                 title="LOA Ended",
-                description=f"**User: ** <@{user.mention}>\n**Start Time: ** {discord.utils.format_dt(loa_record.get('start_date'))}\n**End Date: ** {discord.utils.format_dt(loa_record.get('end_date'))}\n**End Reason: ** Auto Ended",
+                description=(
+                    f"**User:** {user.mention}\n"
+                    f"**Start Time:** {discord.utils.format_dt(record.get('start_date'))}\n"
+                    f"**End Date:** {discord.utils.format_dt(record.get('end_date'))}\n"
+                    f"**End Reason:** Auto Ended"
+                ),
                 color=discord.Color.light_grey()
             )
 
-            await channel.send(embed=log_embed)
+            await channel.send(embed=embed)
 
-            await user.send(f"Your LOA in **{guild.name}** has **ENDED**!")
+            # DM user
+            try:
+                await user.send(
+                    f"Your LOA in **{guild.name}** has **ENDED**!"
+                )
+            except discord.Forbidden:
+                pass
+
+            await self._cleanup_record(record)
+
+    async def _cleanup_record(self, record: dict):
+        """Archive and delete an LOA record safely."""
+        await stored_loa.insert_one(record)
+        await loa.delete_one({"_id": record["_id"]})
+
+    @check_loa_end_date.before_loop
+    async def before_check_loa_end_date(self):
+        await self.bot.wait_until_ready()
+
 
 async def setup(bot: commands.Bot):
     await bot.add_cog(LOAEvent(bot))
