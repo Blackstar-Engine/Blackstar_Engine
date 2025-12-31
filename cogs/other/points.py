291e4c24563df844b01d58eb4d3681b1981fed8b
JustPhantom50 <tjherm4441@gmail.com>
2025-12-31 06:27:44 +0000

    changed points system

--- cogs/other/points.py
+++ cogs/other/points.py
@@ -1,6 +1,6 @@
 import discord
 from discord.ext import commands
-from utils.constants import profiles, foundation_command, site_command, high_command, central_command, ia_id, wolf_id
+from utils.constants import profiles, foundation_command, site_command, high_command, central_command, ia_id, wolf_id, departments
 
 class AcceptDenyButtons(discord.ui.View):
     def __init__(self, bot, user, points, embed, profile):
@@ -96,6 +96,33 @@ class AcceptDenyButtons(discord.ui.View):
         await self.user.send(f"Your points request for **{self.points}** in **{interaction.guild.name}** has been **DENIED**!")
         await interaction.response.edit_message(content=None, view=None, embed=self.embed)
 
+class UnitSelectView(discord.ui.View):
+    def __init__(self, bot, options, profile):
+        super().__init__(timeout=300)
+        self.bot = bot
+        self.profile = profile
+
+        self.dept = None
+
+        self.dept_role_select.options = options
+    
+    @discord.ui.select(
+        placeholder="Select a Role",
+        min_values=1,
+        max_values=1,
+        options=[]
+    )
+    async def dept_role_select(self, interaction: discord.Interaction, select: discord.ui.Select):
+        await interaction.response.defer(ephemeral=True)
+        value = select.values[0]
+
+        if value == "no_units":
+            self.dept = "no_unit"
+        else:
+            self.dept = value
+
+        self.stop()
+
 class Points(commands.Cog):
     def __init__(self, bot: commands.Bot):
         self.bot = bot
@@ -115,6 +142,29 @@ class Points(commands.Cog):
         
         profile = await profiles.find_one({"guild_id": ctx.guild.id, "user_id": ctx.author.id})
 
+        options = []
+        units = profile.get("unit")
+
+        for unit, data in units.items():
+            if data.get("is_active"):
+                options.append(discord.SelectOption(label=unit))
+        
+        if options == []:
+            options.append(discord.SelectOption(label="No Active Units", value="no_units"))
+
+        unit_select_view = UnitSelectView(self.bot, options, profile)
+        unit_select_embed = discord.Embed(title="Select a Unit", description="Please select a unit you would like this point request to send to!", color=discord.Color.light_grey())
+
+        await ctx.send(embed=unit_select_embed, view=unit_select_view)
+
+        await unit_select_view.wait()
+
+        dept = unit_select_view.dept
+
+        channel_id = await departments.find_one({"display_name": dept})
+
+        channel_var = ctx.guild.get_channel(int(channel_id.get("points_request_channel")))
+
         mod_embed = discord.Embed(title="New Points Request",
                                   description=f"**User: ** {ctx.author.mention}\n** Requested Points: ** {points}\n**Proof: ** {channel}",
                                   color=discord.Color.light_grey())
@@ -123,10 +173,9 @@ class Points(commands.Cog):
                             value=f"> **Codename: ** {profile.get('codename')}\n> **Rank: ** {profile.get('rank')}\n> **Join Date: ** {profile.get('join_date')}\n> **Current Points: ** {profile.get('current_points')}\n> **Total Points: ** {profile.get('total_points')}")
 
 
-        view = AcceptDenyButtons(self.bot, ctx.author, points, mod_embed, profile)
+        accept_deny_buttons_view = AcceptDenyButtons(self.bot, ctx.author, points, mod_embed, profile)
 
-        channel = await self.bot.fetch_channel(1453154400989085738)
-        await channel.send(embed=mod_embed, view=view)
+        await channel_var.send(embed=mod_embed, view=accept_deny_buttons_view)
 
         await ctx.send(embed=embed)
 
