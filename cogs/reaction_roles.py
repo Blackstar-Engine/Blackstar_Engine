b2cfff9a1580c84e0fdfb33ff4cb9578f24bf2a2
JustPhantom50 <tjherm4441@gmail.com>
2026-02-01 16:51:02 +0000

    RR Emojis

--- cogs/reaction_roles.py
+++ cogs/reaction_roles.py
@@ -11,13 +11,13 @@ class ReactionRoles(commands.Cog):
         embed = discord.Embed(
             title="Blackstar Role Selection",
             description="The following are roles you can recieve notifications for: \n\n"
-                        "> 📢 | **Annoucements Ping: ** Main Annoucements\n"
-                        "> **Poll Ping: ** Polls\n"
-                        "> **MISC Ping: ** Miscellaneous\n"
-                        "> **Game Night Ping: ** Game Nights\n"
-                        "> **Question Ping: ** Question of the Day\n"
-                        "> **Vote Ping: ** Votes\n"
-                        "> **Chat Revive Ping: ** Chat Revival\n\n"
+                        "> <:BlackStar_Announcement:1467561153499631738> | **Annoucements Ping: ** Main Annoucements\n"
+                        "> <:BlackStar_Poll:1467561281698660565> | **Poll Ping: ** Polls\n"
+                        "> <:BlackStar_Miscellaneous:1467561252120166533> | **MISC Ping: ** Miscellaneous\n"
+                        "> <:BlackStar_Gamenight:1467561216720240831> | **Game Night Ping: ** Game Nights\n"
+                        "> <:BlackStar_QOTD:1467561311398268938> | **Question Ping: ** Question of the Day\n"
+                        "> <:BlackStar_Vote:1467561338321502348> | **Vote Ping: ** Votes\n"
+                        "> <:BlackStar_ChatRevive:1467561189612716205> | **Chat Revive Ping: ** Chat Revival\n\n"
                         "*Select a role below to add or remove the role.*",
             color=discord.Color.light_grey()
         )
