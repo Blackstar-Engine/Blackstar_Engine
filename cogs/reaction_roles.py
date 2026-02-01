a3b8db6f6cd4da36b8c37fc3b1d26b692730a98a
JustPhantom50 <tjherm4441@gmail.com>
2026-02-01 16:57:08 +0000

    RR foundation locked

--- cogs/reaction_roles.py
+++ cogs/reaction_roles.py
@@ -1,6 +1,7 @@
 import discord
 from discord.ext import commands
 from ui.reaction_roles.views.RoleSelect import RoleSelect
+from utils.constants import foundation_command
 
 class ReactionRoles(commands.Cog):
     def __init__(self, bot: commands.Bot):
@@ -8,6 +9,12 @@ class ReactionRoles(commands.Cog):
         
     @commands.command(name="send-reactions", description="This will send an embed with all reaction roles for the server.")
     async def send_reaction_roles(self, ctx: commands.Context):
+        foundation_role = await ctx.guild.fetch_role(foundation_command)
+
+        if foundation_role not in ctx.author.roles:
+            return
+        
+        await ctx.message.delete()
         embed = discord.Embed(
             title="Blackstar Role Selection",
             description="The following are roles you can recieve notifications for: \n\n"
