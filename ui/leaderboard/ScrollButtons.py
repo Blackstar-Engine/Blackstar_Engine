8b2be43c184c6a13a60743d8d1ccf9165eb7696d
optionest <optionest12@gmail.com>
2026-03-04 21:51:20 -0600

    partial leaderboard addition

--- ui/leaderboard/ScrollButtons.py
+++ ui/leaderboard/ScrollButtons.py
@@ -8,13 +8,13 @@ class LeaderboardView(View):
         self.current_page = 0
         self.max_page = len(pages)
         self.update_buttons()
-    @discord.ui.button(label="<---", style=discord.ButtonStyle.secondary)
+    @discord.ui.button(label="←", style=discord.ButtonStyle.secondary)
     async def prev_button(self, interaction: discord.Interaction, button: Button):
         if self.current_page > 0:
             self.current_page -= 1
         self.update_buttons()
         await interaction.response.edit_message(embed=self.get_embed(), view=self)
-    @discord.ui.button(label="--->", style=discord.ButtonStyle.secondary)
+    @discord.ui.button(label="→", style=discord.ButtonStyle.secondary)
     async def next_button(self, interaction: discord.Interaction, button: Button):
         if self.current_page < self.max_page - 1:
             self.current_page += 1
@@ -34,8 +34,7 @@ class LeaderboardView(View):
                 page, start=self.current_page * per_page + 1
             )
         ]
-        return discord.Embed(
-            title=f"Point Leaderboard (Page {self.current_page + 1}/{self.max_page})",
-            description="\n".join(lines),
-            color=discord.Color.light_grey(),
-        )
+
+        embed = discord.Embed(title=f"Point Leaderboard (Page {self.current_page + 1}/{self.max_page})", description="\n".join(lines), color=discord.Color.light_grey())
+        embed.set_thumbnail(url="https://cdn.discordapp.com/avatars/1450302678524756040/3557930241bf8360a9535a5f27d42cf4.png?size=1024")
+        return embed
