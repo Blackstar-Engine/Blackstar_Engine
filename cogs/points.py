e687e3d4d1c77da87dd299ca23c2ba8fddc37e09
optionest <optionest12@gmail.com>
2026-03-14 18:48:28 -0500

    added point gifting

--- cogs/points.py
+++ cogs/points.py
@@ -2,9 +2,10 @@ import discord
 from discord.ext import commands
 from ui.points.views.AcceptDenyButtons import PointsRequestView
 from ui.points.views.UnitSelect import UnitSelectView
-from utils.utils import fetch_profile, fetch_unit_options, fetch_department, generate_timestamp
+from utils.utils import fetch_profile, fetch_unit_options, fetch_department, generate_timestamp, fetch_id, has_approval_perms
 import uuid
-from utils.constants import point_requests
+from utils.constants import point_requests, profiles
+from datetime import datetime
 
 async def send_points_request(channel, profile, dept_name, points, proof):
     request_id = str(uuid.uuid4())
@@ -86,6 +87,101 @@ class Points(commands.Cog):
         view.add_item(container)
 
         await select_message.edit(view=view)
+
+    @points.command(name="gift", description="Gift points to other units")
+    async def gift(self, ctx: commands.Context, user: discord.Member, points: int, *, reason: str):
+        results = await fetch_id(
+            ctx.guild.id,
+            ["central_command", "foundation_command", "high_command", "site_command"]
+        )
+
+        allowed_roles = [
+            results["central_command"],
+            results["foundation_command"],
+            results["high_command"],
+            results["site_command"]
+        ]
+        if any(role.id in allowed_roles for role in ctx.author.roles):
+
+            if ctx.author == user:
+                await ctx.send("You can not gift points to yourself!", ephemeral=True)
+                return
+
+            limit = None
+
+            if ctx.guild.get_role(results["central_command"]) in ctx.author.roles:
+                limit = 1
+            elif ctx.guild.get_role(results["high_command"]) in ctx.author.roles:
+                limit = 2
+            elif ctx.guild.get_role(results["site_command"]) in ctx.author.roles:
+                limit = 3
+            elif ctx.guild.get_role(results["foundation_command"]) in ctx.author.roles:
+                if ctx.author.id == 1371489554279825439:
+                    limit = 999999999999999
+                else:
+                    limit = 5
+
+            profile = await fetch_profile(ctx)
+   
+            
+            if points <= 0:
+                await ctx.send("You must gift a positive number of points.", ephemeral=True)   
+                return
+            
+            if not profile.get("gifted", {}):
+                await profiles.update_one(
+                    {"guild_id": ctx.guild.id, "user_id": ctx.author.id},
+                    {"$set": {"gifted": {"current_month": datetime.now().month, "gifted_points": 0}}}
+                )
+
+            stored_month = profile.get("gifted", {}).get("current_month")
+            if stored_month != datetime.now().month:
+                await profiles.update_one(
+                    {"guild_id": ctx.guild.id, "user_id": ctx.author.id},
+                    {"$set": {"gifted": {"current_month": datetime.now().month, "gifted_points": 0}}}
+                )
+
+            profile = await fetch_profile(ctx)
+            gifted_total = profile.get("gifted", {}).get("gifted_points")
+            if gifted_total + points > limit:
+                await ctx.send("You cannot gift that amount of points because it exceeds your remaining gifting limit.", ephemeral=True)      
+                return              
+
+            profile = await fetch_profile(ctx)
+            unit_select_view = UnitSelectView(self.bot, fetch_unit_options(profile), profile)
+            msg = await ctx.send(view=unit_select_view, ephemeral=True)
+            await unit_select_view.wait()
+
+            await ctx.send('Gift succesfully sent!', ephemeral=True)
+
+            dept = unit_select_view.dept
+
+            if not dept or dept == "no_unit":
+                return
+            
+            department_doc = await fetch_department(ctx, dept)
+            if not department_doc:
+                await ctx.send("I cannot find the correct channel.", ephemeral=True)
+
+            channel = ctx.guild.get_channel(int(department_doc.get("points_request_channel")))
+
+
+            await profiles.update_one(
+                {"guild_id": ctx.guild.id, "user_id": ctx.author.id},
+                {"$inc": {"gifted.gifted_points": points}}
+            )        
+
+            await profiles.update_one(
+                {"guild_id": ctx.guild.id, "user_id": user.id},
+                {"$inc": {f"unit.{dept}.current_points": points}}
+            )  
+            
+            embed = discord.Embed(description=f"## Point Gift\n> **Point Gifter:** {ctx.author.mention}\n> **Points:** {points}\n> **Gifted To:** {user.mention}\n> **Reason:** {reason}", color=discord.Color.green())
+            await channel.send(embed=embed)
+
+        else:
+            return await ctx.send("You are not allowed to use this command!", ephemeral=True)
+        
 
 async def setup(bot: commands.Bot):
     await bot.add_cog(Points(bot))
