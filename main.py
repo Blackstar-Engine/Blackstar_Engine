f0424b415c972b1894ca2c26d94fa6afa3a20fe9
JustPhantom50 <tjherm4441@gmail.com>
2026-03-25 02:53:28 +0000

    enlistment reminders, big fixes, tts voice

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ import os
 import sys
 import asyncio
 from collections import defaultdict
-from utils.constants import BlackstarConstants, auto_replys, enlistment_requests, point_requests, promotion_requests, whitelisted_guilds
+from utils.constants import BlackstarConstants, auto_replys, enlistment_requests, point_requests, promotion_requests, whitelisted_guilds, logger, discord_http_logger, discord_logger
 from ui.promotion.views.PromotionRequest import PromotionRequestView
 from ui.points.views.AcceptDenyButtons import PointsRequestView
 from ui.enlistment_request.views.EnlistmentRequestView import EnlistmentRequestView
@@ -58,45 +58,45 @@ class Bot(commands.Bot):
                     try:
                         await bot.load_extension(f"cogs.{cog_module}")
                         cog_counter += 1
-                        print(f"{cog_module} loaded successfully")
+                        logger.info(f"{cog_module} loaded successfully")
                     except Exception as e:
-                        print(f"{cog_module} failed to load: {e}")
+                        logger.error(f"{cog_module} failed to load: {e}")
 
-        print(f"Successfully loaded {cog_counter} cog(s)")
+        logger.info(f"Successfully loaded {cog_counter} cog(s)")
 
         async for req in enlistment_requests.find({"is_active": True}):
             view = EnlistmentRequestView(req["_id"], req["snapshot"])
             self.add_view(view)
             enlistment_counter += 1
         
-        print(f"Successfully loaded {enlistment_counter} enlistments")
+        logger.info(f"Successfully loaded {enlistment_counter} enlistments")
 
         async for req in point_requests.find({"is_active": True}):
             view = PointsRequestView(req["_id"], req["snapshot"])
             self.add_view(view)
             points_counter += 1
         
-        print(f"Successfully loaded {points_counter} points")
+        logger.info(f"Successfully loaded {points_counter} points")
 
         async for req in promotion_requests.find({"is_active": True}):
             view = PromotionRequestView(self, req["_id"], req["snapshot"])
             self.add_view(view)
             promotion_counter += 1
         
-        print(f"Successfully loaded {promotion_counter} promotions")
+        logger.info(f"Successfully loaded {promotion_counter} promotions")
 
     async def on_connect(self):
-        print('Connected to discord gateway')
+        discord_http_logger.info('Connected to discord gateway')
     
     async def on_disconnected(self):
-        print('Disconnected from discord gateway')
+        discord_http_logger.error('Disconnected from discord gateway')
 
     async def on_shard_connect(self, shard_id: int):
         await self.tree.sync()
-        print(f'Shard {shard_id} has connected to discord gateway')
+        discord_http_logger.info(f'Shard {shard_id} has connected to discord gateway')
     
     async def on_shard_disconnected(self, shard_id: int):
-        print(f'Shard {shard_id} has disconnected from discord gateway')
+        discord_http_logger.error(f'Shard {shard_id} has disconnected from discord gateway')
             
 
     @watch(path='cogs', preload=False)
@@ -106,7 +106,7 @@ class Bot(commands.Bot):
         records = await auto_replys.find().to_list(length=None)
         for record in records:
             bot.auto_replys.append(record)
-        print(f"All {len(bot.auto_replys)} auto-replys loaded")
+        logger.info(f"All {len(bot.auto_replys)} auto-replys loaded")
 
         bot.tts_queues = defaultdict(asyncio.Queue)
         bot.tts_tasks = {}
@@ -115,17 +115,17 @@ class Bot(commands.Bot):
 
         for guild in bot.guilds:
             if guild.id not in whitelisted_guilds:
-                print(f"Server not found: {guild.name}({guild.id})")
+                logger.error(f"Server not found: {guild.name}({guild.id})")
                 try:
                     await guild.leave()
                 except Exception:
                     await guild.owner.send(f"Please remove me from **{guild.name}**, I will not work!")
                 
             else:
-                print(f'Chunked: {guild.id}')
+                logger.info(f'Chunked: {guild.id}')
                 await guild.chunk()
         
-        print(f'{self.user} is ready.')
+        logger.info(f'{self.user} is ready.')
 
 bot = Bot()
 
@@ -136,24 +136,24 @@ async def start_bot():
 
     while retries < max_retries:
         try:
-            print(f'Starting bot... (Attempt {retries + 1})')
+            discord_logger.info(f'Starting bot... (Attempt {retries + 1})')
             await bot.start(constants.TOKEN)
         except (TimeoutError) as e:
             retries += 1
-            print(f'Connection error occured. Thrown error: {e}')
+            discord_logger.error(f'Connection error occured. Thrown error: {e}')
 
             if retries < max_retries:
-                print(f'Retrying in {retry_delay} seconds...')
+                discord_logger.info(f'Retrying in {retry_delay} seconds...')
                 await asyncio.sleep(retry_delay)
             else:
                 break
 
         except Exception as e:
-            print(f'Unexpected error occured. {e}')
+            discord_logger.error(f'Unexpected error occured. {e}')
             sys.exit('FAILED TO START: UNEXPECTED ERROR')
 
     
-    print('Max retries reached - stopping bot...')
+    discord_logger.critical('Max retries reached - stopping bot...')
     sys.exit('FAILED TO START: MAX RETRIES')
             
 
@@ -161,5 +161,5 @@ if __name__ == '__main__':
     try:
         asyncio.run(start_bot())
     except KeyboardInterrupt:
-        print('Bot shutting down...')
+        discord_logger.info('Bot shutting down...')
         sys.exit(0)
